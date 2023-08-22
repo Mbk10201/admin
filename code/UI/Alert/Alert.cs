e34a33647e8e2ad68fe25aa7e500e4930d083ec1
MBK <74474524+Mbk10201@users.noreply.github.com>
2023-08-23 00:10:26 +0200

    Added alert (Notification) system

---
+++ code/UI/Alert/Alert.cs
@@ -0,0 +1,35 @@
+﻿using Sandbox;
+using System.Linq;
+
+namespace Mbk.Admin.UI.Alert;
+
+public partial class Alert
+{
+	public static AdminAlertPanel AlertPanel { get; set; }
+
+	[ClientRpc]
+	public static void Add( AlertBuilder item )
+	{
+		AlertPanel.Add( item );
+	}
+
+	[ClientRpc]
+	public static void Add( string title, string message, eAlertType type )
+	{
+		AlertPanel.Add( new()
+		{
+			Title = title,
+			Message = message,
+			Type = type
+		} );
+	}
+
+	[GameEvent.Client.Frame]
+	public static void Frame()
+	{
+		if ( Game.RootPanel.Children.OfType<AdminAlertPanel>().Count() == 0 )
+		{
+			AlertPanel = Game.RootPanel.AddChild<AdminAlertPanel>();
+		}
+	}
+}
---
+++ code/UI/Alert/AlertBuilder.cs
@@ -0,0 +1,17 @@
+﻿namespace Mbk.Admin.UI.Alert;
+
+public enum eAlertType
+{
+	Info,
+	Success,
+	Error,
+	Warning,
+	Custom
+}
+
+public class AlertBuilder
+{
+	public string Title { get; set; }
+	public string Message { get; set; }
+	public eAlertType Type { get; set; }
+}
