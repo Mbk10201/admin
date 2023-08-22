99013e4bfe9b5d9fe52c108841eea7e2884ec397
MBK <74474524+Mbk10201@users.noreply.github.com>
2023-08-23 00:12:55 +0200

    Added: Default & Can be removed

--- code/Role.cs
+++ code/Role.cs
@@ -8,17 +8,17 @@ public partial class Role : BaseNetworkable
 {
 
 	/// <summary>
-	/// The unique ID of this role
+	/// The unique ID of this role.
 	/// </summary>
 	[Net] public long Id { get; set; }
 
 	/// <summary>
-	/// The name of the role
+	/// The name of the role.
 	/// </summary>
 	[Net] public string Name { get; set; }
 
 	/// <summary>
-	/// The list of permissions id that this role has
+	/// The list of permissions id that this role has.
 	/// </summary>
 	[Net] public IList<long> Permissions { get; private set; }
 
@@ -30,7 +30,7 @@ public partial class Role : BaseNetworkable
 	[Net] public int ImmunityLevel { get; private set; }
 
 	/// <summary>
-	///	The role icon (filepath)
+	///	The role icon (filepath).
 	/// </summary>
 	[Net] public string Icon { get; set; }
 
@@ -44,16 +44,28 @@ public partial class Role : BaseNetworkable
 	/// </summary>
 	[Net] public bool Mentionable { get; set; }
 
+	/// <summary>
+	/// If this role can be removed
+	/// </summary>
+	[Net] public bool CanBeRemoved { get; set; } = true;
+
+	/// <summary>
+	/// If this role is the default one when a new player join
+	/// Warning, there should be only one in the instance !!
+	/// </summary>
+	[Net] public bool Default { get; set; } = false;
+
 	public Role() 
 	{
 		Permissions = new List<long>();
 	}
 
-	public Role( string name, int immunitylevel = 25 ) : this()
+	public Role( string name, int immunitylevel = 25, bool canberemoved = true ) : this()
 	{
 		Id = Game.Random.NextInt64();
 		Name = name;
 		ImmunityLevel = immunitylevel;
+		CanBeRemoved = canberemoved;
 
 		AdminSystem.Instance.Roles.Add( this );
 	}
@@ -74,6 +86,9 @@ public partial class Role : BaseNetworkable
 		ImmunityLevel = immunity;
 	}
 
+	/// <summary>
+	///	Return a list of permissions object from id references of the role.
+	/// </summary>
 	public IList<Permission> GetPermissionsByRef()
 	{
 		IList<Permission> list = new List<Permission>();
@@ -89,9 +104,24 @@ public partial class Role : BaseNetworkable
 		return list;
 	}
 
+	/// <summary>
+	///	Return a role object from id reference.
+	/// </summary>
 	public static Role GetRef( long id ) => AdminSystem.Instance.Roles.SingleOrDefault( x => x.Id == id );
+
+	/// <summary>
+	///	Return a role object from a name reference.
+	/// </summary>
 	public static Role GetRef( string name ) => AdminSystem.Instance.Roles.SingleOrDefault( x => x.Name == name );
 
+	/// <summary>
+	///	Return the default role 
+	/// </summary>
+	public static Role GetDefault( ) => AdminSystem.Instance.Roles.SingleOrDefault( x => x.Default );
+
+	/// <summary>
+	///	Return a role object list from a list id reference
+	/// </summary>
 	public static IList<Role> GetRef( IList<long> list)
 	{
 		IList<Role> roles = new List<Role>();
@@ -102,6 +132,9 @@ public partial class Role : BaseNetworkable
 		return roles;
 	}
 
+	/// <summary>
+	///	Return a role object list from a list name reference
+	/// </summary>
 	public static IList<Role> GetRef( IList<string> list )
 	{
 		IList<Role> roles = new List<Role>();
@@ -112,6 +145,9 @@ public partial class Role : BaseNetworkable
 		return roles;
 	}
 
+	/// <summary>
+	///	Create a role
+	/// </summary>
 	[ConCmd.Server("createrole")] 
 	public static void Create( string name, int immunity = 25 ) 
 	{ 
@@ -119,6 +155,9 @@ public partial class Role : BaseNetworkable
 		AdminSystem.SaveRoles();
 	}
 
+	/// <summary>
+	///	Update a role name
+	/// </summary>
 	[ConCmd.Server("updatename")] 
 	public static void UpdateName( long roleid, string name ) 
 	{ 
@@ -126,6 +165,9 @@ public partial class Role : BaseNetworkable
 		AdminSystem.SaveRoles();
 	}
 
+	/// <summary>
+	///	Delete a role by id
+	/// </summary>
 	[ConCmd.Server("deleterole")] 
 	public static void Delete( long roleid ) 
 	{ 
@@ -133,6 +175,9 @@ public partial class Role : BaseNetworkable
 		AdminSystem.SaveRoles();
 	}
 
+	/// <summary>
+	///	Delete a role by name
+	/// </summary>
 	[ConCmd.Server("deleterole")] 
 	public static void Delete( string rolename ) 
 	{ 
@@ -140,6 +185,9 @@ public partial class Role : BaseNetworkable
 		AdminSystem.SaveRoles();
 	}
 
+	/// <summary>
+	///	Delete a permission from the role by id
+	/// </summary>
 	[ConCmd.Server("removepermission")] 
 	public static void RemovePermission( long roleid, long permid ) 
 	{ 
@@ -147,6 +195,9 @@ public partial class Role : BaseNetworkable
 		AdminSystem.SaveRoles();
 	}
 
+	/// <summary>
+	///	Add a permission to the role.
+	/// </summary>
 	[ConCmd.Server("addpermission")] 
 	public static void AddPermission( long roleid, long permid ) 
 	{ 
