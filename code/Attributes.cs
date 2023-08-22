6eb588e4ff325f1796e82cb11315ed2873c11657
MBK <74474524+Mbk10201@users.noreply.github.com>
2023-08-23 00:14:18 +0200

    Added var to permit to hide the command on the UI

--- code/Attributes.cs
+++ code/Attributes.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Mbk.Admin.UI.Dialog;
+using Mbk.Admin.UI.Dialog.Childs;
+using System;
 using System.ComponentModel.DataAnnotations;
 
 namespace Mbk.Admin;
@@ -9,12 +11,14 @@ public class CommandAttribute : Attribute
 	public string Key { get; set; }
 	public string Icon { get; set; }
 	public bool ClientAction { get; set; }
+	public bool DisplayInUI { get; set; }
 	public string[] Aliases { get; }
 	public string Description { get; set; }
+	public Type DialogType { get; set; }
 	internal string GeneratedFrom = "";
 	internal bool IsGenerated => !string.IsNullOrEmpty( GeneratedFrom );
 
-	public CommandAttribute( string key, string icon = "star", bool clientaction = false, params string[] aliases)
+	public CommandAttribute( string key, Type dialog, string icon = "material-symbols:360", bool clientaction = false, bool displayinui = true, params string[] aliases)
 	{
 		if ( string.IsNullOrEmpty( key ) )
 			throw new ArgumentNullException( nameof( key ) );
@@ -23,5 +27,7 @@ public class CommandAttribute : Attribute
 		Icon = icon;
 		ClientAction = clientaction;
 		Aliases = aliases;
+		DialogType = dialog;
+		DisplayInUI = displayinui;
 	}
 }
